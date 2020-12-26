a825ec8c1caeaa973c24b86d8ae2bf566dc74928
Josemi <josemijuanes@gmail.com>
2020-12-26 20:58:37 +0100

    Move rollup configuration file to siimple-components package folder and add this file to .npmignore

--- packages/siimple-components/rollup.config.js
+++ packages/siimple-components/rollup.config.js
@@ -1,7 +1,8 @@
 let path = require("path");
-let paths = require("./config/paths.js");
+let paths = require("../../paths.js");
 let {terser} = require("rollup-plugin-terser");
 let cleanup = require("rollup-plugin-cleanup");
+let {babel} = require("@rollup/plugin-babel");
 let ignoreImport = require("rollup-plugin-ignore-import");
 
 //Generate the banner
@@ -13,6 +14,7 @@ banner.push(" */");
 banner.push("");
 
 //Output bundle extensions
+let name = "siimple-components";
 let outputBundleExtensions = ["umd", "umd.min", "esm", "esm.min"].map(function (ext) {
     return `${ext}.js`; //Add js extension
 });
@@ -52,35 +54,23 @@ let createMainConfig = function (inputFile, outputFile, outputName) {
     return config;
 };
 
-//Create the lib configuration
-let createLibConfig = function (inputFile, outputFile) {
-    //Initialize the configuration object
-    let config = {
-        "input": path.join(paths.packages.neutrine, inputFile),
-        "output": {
-            "format": "esm",
-            "file": path.join(paths.packages.neutrine, "lib", `${outputFile}.js`),
-            "globals": {"react": "React", "react-dom": "ReactDOM"},
-            "plugins": [terser()],
-            "banner": banner.join("\n")
-        },
-        "external": ["react", "react-dom"],
-        "plugins": [
-            ignoreImport({"extensions": [".scss", ".css"]}),
-            cleanup()
-        ]
-    };
-    //Return the configutation
-    return config;
+//Create the configuration for siimple-components
+module.exports = {
+    "input": path.join(paths.packages.folder, name, "src", "index.js"),
+    "output": {
+        "format": "umd",
+        "file": path.join(paths.packages.folder, name, "dist", `${name}.js`),
+        "globals": {"react": "React", "react-dom": "ReactDOM"},
+        "extend": true,
+        "name": "Siimple",
+        "plugins": [terser()],
+        "banner": banner.join("\n")
+    },
+    "external": ["react", "react-dom"],
+    "plugins": [
+        ignoreImport({"extensions": [".scss", ".css"]}),
+        babel({"exclude": "node_modules/**"}),
+        cleanup()
+    ]
 };
-
-//Export the configuration object
-module.exports = [
-    //createMainConfig(path.join(__dirname, "./src/components/index.js"), "neutrine", "Neutrine"),
-    createLibConfig("./src/components/index.js", "components"),
-    createLibConfig("./src/datatable/index.js", "datatable"),
-    createLibConfig("./src/editor/index.js", "editor"),
-    createLibConfig("./src/widgets/index.js", "widgets"),
-    createLibConfig("./src/utils/index.js", "utils")
-];
 
