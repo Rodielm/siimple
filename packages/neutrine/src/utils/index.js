385cee6d6fd763b21a18185c901224708a1ccd9b
Josemi <josemijuanes@gmail.com>
2021-01-31 11:57:50 +0100

    Clean packages folder

--- packages/neutrine/src/utils/index.js
+++
@@ -1,5 +0,0 @@
-export {classNames} from "./classnames.js";
-export {global, createGlobalStorage} from "./global.js";
-export {whenReady} from "./ready.js";
-export {createStorage, Storage} from "./storage.js";
-
