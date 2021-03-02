8b99d8afb9e203751eadc26f1d17ac9569a6c26e
Josemi <josemijuanes@gmail.com>
2021-03-02 22:49:03 +0100

    components: fixed modal styles, removed ModalTitle component and rewrite ModalWrapper

--- packages/siimple-components/src/experiments/Modal.js
+++ packages/siimple-components/src/experiments/Modal.js
@@ -10,7 +10,7 @@ export const Modal = function (props) {
     let classList = ["siimple-modal"];
     //Check for modal predefined size
     if (typeof props.size === "string") {
-        classList.push("siimple-modal--" + props.size.toLowerCase());
+        classList.push("is-" + props.size.toLowerCase());
     }
     //Merge the modal class names
     newProps.className = classNames(classList, props.className);
@@ -24,10 +24,10 @@ Modal.defaultProps = {
 };
 
 //Other Modal componens
-export const ModalContent = createHtmlElement("div", "siimple-modal-content");
+//export const ModalContent = createHtmlElement("div", "siimple-modal-content");
 export const ModalHeader = createHtmlElement("div", "siimple-modal-header");
-export const ModalHeaderTitle = createHtmlElement("div", "siimple-modal-header-title");
-export const ModalHeaderClose = createHtmlElement("div", "siimple-modal-header-close");
+//export const ModalHeaderTitle = createHtmlElement("div", "siimple-modal-header-title");
+export const ModalClose = createHtmlElement("div", "siimple-modal-close");
 export const ModalBody = createHtmlElement("div", "siimple-modal-body");
 export const ModalFooter = createHtmlElement("div", "siimple-modal-footer");
 
@@ -35,13 +35,11 @@ export const ModalFooter = createHtmlElement("div", "siimple-modal-footer");
 export const ModalWrapper = function (props) {
     return (
         <Modal size={props.size}>
-            <ModalContent>
-                <ModalHeader>
-                    <ModalHeaderTitle>{props.title}</ModalHeaderTitle>
-                    <ModalHeaderClose onClick={props.onClose} />
-                </ModalHeader>
-                {props.children}
-            </ModalContent>
+            <ModalHeader>
+                <span>{props.title}</span>
+                <ModalClose onClick={props.onClose} />
+            </ModalHeader>
+            {props.children}
         </Modal>
     );
 };
