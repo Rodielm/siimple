61fad9926ef8f28f94230ee5fb27ae3547ae3eea
Josemi <josemijuanes@gmail.com>
2021-05-29 00:33:03 +0200

    Upgrade script for building SVG sprites

--- scripts/build-svg-sprites.js
+++ scripts/build-svg-sprites.js
@@ -1,39 +1,39 @@
-let fs = require("fs");
-let path = require("path");
-let svgo = require("svgo");
-let svgstore = require("svgstore");
-let getArgs = require("get-args");
-let paths = require("../paths.js");
+const fs = require("fs");
+const path = require("path");
+const {optimize} = require("svgo");
+const svgstore = require("svgstore");
+const getArgs = require("get-args");
+const paths = require("../paths.js");
 
 //Build the svg sprite
 process.nextTick(function () {
-    let options = getArgs().options;
+    const options = getArgs().options;
     if (typeof options.output !== "string") {
         throw new Error("No output file provided");
     }
     //Get all icons
-    let files = fs.readdirSync(paths.icons.folder, "utf8").filter(function (file) {
+    const files = fs.readdirSync(paths.icons.images, "utf8").filter(function (file) {
         return path.extname(file) === ".svg"; //Get only .svg files
     });
     //Sprites storage
-    let sprites = svgstore();
-    let parseSvgFile = function (index) {
-        if (index >= files.length) {
-            //Write the sprites to the file and exit
-            return fs.writeFileSync(options.output, sprites, "utf8");
-        }
-        //Get current file
-        let file = files[index];
-        let content = fs.readFileSync(path.join(paths.icons.folder, file), "utf8");
+    const sprites = svgstore();
+    files.forEach(function (file, index) {
+        const content = fs.readFileSync(path.join(paths.icons.images, file), "utf8");
         //Initialize the svg minimize
-        let prefix = path.basename(file, path.extname(file));
-        let svgmin = new svgo({plugins: [{cleanupIDs: {prefix: prefix + '-', minify: true}}]});
-        return svgmin.optimize(content).then(function (result) {
-            sprites.add(prefix, result.data); //Add to the sprite
-            return parseSvgFile(index + 1); //Next svg
+        const prefix = path.basename(file, path.extname(file));
+        const result = optimize(content, {
+            "plugins": [{
+                "name": "cleanupIDs",
+                "params": {
+                    "prefix": prefix + "-",
+                    "minify": true
+                }
+            }]
         });
-    };
-    //Parse all svg files
-    return parseSvgFile(0);
+        //Save this sprite
+        return sprites.add(prefix, result.data);
+    });
+    //Write the sprites to the file and exit
+    return fs.writeFileSync(options.output, sprites.toString(), "utf8");
 });
 
